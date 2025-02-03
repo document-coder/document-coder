38e2740dc8baaa680ab46f3ee724179c7631b143
David Stein <davidbstein@gmail.com>
2025-02-02 16:45:35 -0800

    removing accidental commit

--- annotation_tool/config/settings/base.py
+++ annotation_tool/config/settings/base.py
@@ -78,8 +78,7 @@ REST_FRAMEWORK = {
   'DEFAULT_AUTHENTICATION_CLASSES': [
       'rest_framework.authentication.SessionAuthentication',
       'rest_framework.authentication.TokenAuthentication',
-  ],
-  'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+  ]
 }
 
 # Templates
